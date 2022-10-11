749552a70269ce0aea93be83985140130001d436
McHoundi <petu.esk@gmail.com>
2022-10-11 12:44:43 +0300

    imported FMOD into project

---
+++ Assets/ScaleChangeTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleChangeTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.localScale = new Vector3(3, 3, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
