f0d431f4e9beb6f8d0f20a5b4a36d4a3f92ad777
McHoundi <petu.esk@gmail.com>
2022-10-11 16:35:09 +0300

    Created an FMOD project inside unity project root

---
+++ Assets/SetPitchParameter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPitchParameter : MonoBehaviour
+{
+
+    FMOD.Studio.EventInstance instance;
+
+    public FMODUnity.EventReference fmodEvent;
+
+    [SerializeField] [Range(0f,10f)]
+    public float pitch;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        instance.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        instance.setParameterByName("pitch", pitch);
+    }
+}
