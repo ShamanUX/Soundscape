9908a6e6e2ea63d067c19fc0271d3ee9a36d4d4c
McHoundi <petu.esk@gmail.com>
2022-10-28 11:55:54 +0300

    melody station demo

---
+++ Assets/SetFMODParameter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFMODParameter : MonoBehaviour
+{
+
+    public FMODUnity.StudioEventEmitter targetEventEmitter;
+
+    public string parameterName;
+    
+}
