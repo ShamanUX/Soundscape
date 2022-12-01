e4bb104c5904063102d7d76730a6be32a19dda37
McHoundi <petu.esk@gmail.com>
2022-12-01 16:15:37 +0200

    Speaker reacts to music - FMODLoudness script

--- Assets/FMODloudness.cs
+++ Assets/FMODloudness.cs
@@ -7,13 +7,13 @@ using FMODUnity;
 public class FMODloudness : MonoBehaviour
 {
 
-
-
     public float[] volumeLevel;
 
     FMOD.ChannelGroup masterChannelGroup;
     FMOD.DSP dsp;
     FMOD.DSP_METERING_INFO dspInfo;
+
+    public GameObject speaker;
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +21,23 @@ public class FMODloudness : MonoBehaviour
         RuntimeManager.CoreSystem.getMasterChannelGroup(out masterChannelGroup);
         masterChannelGroup.getDSP(0, out dsp);
         dsp.setMeteringEnabled(true, true);
-
     }
 
     // Update is called once per frame
     void Update()
     {
         dsp.getMeteringInfo(System.IntPtr.Zero, out dspInfo);
-
         volumeLevel = dspInfo.peaklevel;
+        AnimateSpeaker(volumeLevel);
+        
     }
+
+    void AnimateSpeaker(float[] volumeLevel)
+    {
+        //get avg of channel levels 
+        float avgLevel = (volumeLevel[0] + volumeLevel[1]) / 2;
+
+        speaker.transform.localScale = Vector3.one * avgLevel;
+    }
+
 }
